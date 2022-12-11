43ee60e8c88f0e1cb1c1b3ace40e8f8a7b8d4ed4
Gregory John Casamento <greg.casamento@gmail.com>
2022-12-11 15:35:27 -0500

    Initial Commit

---
+++ NSTableView_ViewBasedCells_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSTableView_ViewBasedCells_test
+//
+//  Created by Gregory Casamento on 12/11/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
